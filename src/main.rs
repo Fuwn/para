afcda1c308c050f197cc90d94c254d8673cb042c
Fuwn <contact@fuwn.me>
2024-06-10 03:11:39 -0700

    refactor: bump nightly again
    I forgot that it's 2024.
    
    It looks like generators were renamed to coroutines.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
 // SPDX-License-Identifier: MIT
 
-#![feature(decl_macro, generators, generator_trait, lazy_cell)]
+#![feature(decl_macro, coroutines, coroutine_trait)]
 #![deny(
   warnings,
   nonstandard_style,
